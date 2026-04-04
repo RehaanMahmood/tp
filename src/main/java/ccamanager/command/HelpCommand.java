737563818917b87e0d6429efd2888df07baa1fbc
yy43yy <yiyang15624@gmail.com>
2026-04-04 10:45:23 +0800

    Update help method

--- src/main/java/ccamanager/command/HelpCommand.java
+++ src/main/java/ccamanager/command/HelpCommand.java
@@ -15,12 +15,15 @@ public class HelpCommand extends Command {
                 "> delete-cca <cca name>\n" +
                 "> add-event <event name>; <cca name>; <data time>\n" +
                 "> add-resident <name>; <matric number>\n" +
+                "> delete-resident <matric number>\n" +
                 "> view-resident\n" +
                 "> add-resident-to-cca <matric number>; <cca name>; <points>\n" +
                 "> add-resident-to-event <matric number>; <event name>; <cca name>\n" +
                 "> view-points\n" +
                 "> cca-stats\n" +
                 "> resident-stats\n" +
+                "> view-cca-events <cca name>\n" +
+                "> view-my-events <matric number>\n" +
                 "> help\n" +
                 "> bye";
         ui.showMessage(help);
